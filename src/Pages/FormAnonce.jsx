75dd968727389b9cda361858ad4c7eb1e0f893ab
khalidek1 <laarbichani098@gmail.com>
2023-05-27 13:25:32 +0100

    add form anonce

---
+++ src/Pages/FormAnonce.jsx
@@ -0,0 +1,418 @@
+import React, { useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
+import axios from "axios";
+import "./FormAnonce.css";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+delete L.Icon.Default.prototype._getIconUrl;
+
+const Form = () => {
+  const center_position = [33.66492, -7.817975];
+  const [markerPosition, setMarkerPosition] = useState({
+    lat: 33.66492,
+    lng: -7.817975,
+  });
+  const handleMapClick = (e) => setMarkerPosition(e.latlng);
+
+  const handleClick = () => {
+    if (markerPosition) {
+      alert(markerPosition.lat, markerPosition.lng);
+    } else {
+      alert("No marker selected.");
+    }
+  };
+
+  const handleFileChange = (event) => {
+    const files = event.target.files;
+    // Do something with the selected files
+    console.log(files);
+  };
+
+  const MapClickEvents = () => {
+    useMapEvents({
+      click: handleMapClick,
+    });
+    return null;
+  };
+
+  const [currentStep, setCurrentStep] = useState(1);
+  const [formData, setFormData] = useState({
+    type: "",
+    // proprieterType: "",
+    latitude: 0,
+    longtitude: 0,
+    prix: 0,
+    surface: 0,
+    nbreChambres: 0,
+    nbreSalleBain: 0,
+    nbreEtages: 0,
+    etat: 0,
+  });
+  const [formErrors, setFormErrors] = useState({});
+
+  // const handleInputChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormData({ ...formData, [name]: value });
+  // };
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleNext = () => {
+    if (validateForm()) {
+      setCurrentStep(currentStep + 1);
+      setFormErrors({});
+    }
+  };
+
+  const handlePrevious = () => {
+    setCurrentStep(currentStep - 1);
+    setFormErrors({});
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    // console.log(Json.stringiformData);
+    if (validateForm()) {
+      //   try {
+      //     const response = await axios.post(
+      //       "http://localhost:8080/Anonce",
+      //       formData
+      //     );
+      //     console.log(response.data); // Handle the response as needed
+      //     }
+      //   catch (error) {
+      //     console.error(error);
+      //   }
+      axios
+        .post("http://localhost:8080/Anonce", formData)
+        .then((response) => {
+          console.log(response.data); // Handle success response
+        })
+        .catch((err) => {
+          console.log(err); // Handle error response
+        });
+    }
+  };
+
+  const validateForm = () => {
+    const errors = {};
+    let isValid = true;
+
+    if (currentStep === 1) {
+      if (!formData.type) {
+        errors.type = "Anonce Type is required";
+        isValid = false;
+      }
+      // if (!formData.proprieterType) {
+      //   errors.proprieterType = "proprieter Type is required";
+      //   isValid = false;
+      // }
+    } else if (currentStep === 2) {
+      if (!formData.prix.trim() === "" && parseFloat(formData.prix) <= 0) {
+        errors.prix = "prix is required";
+        isValid = false;
+      }
+      if (
+        !formData.surface.trim() === "" &&
+        parseFloat(formData.surface) <= 0
+      ) {
+        errors.surface = "surface is required";
+        isValid = false;
+      }
+      if (!formData.nbreChambres.trim() === "") {
+        errors.nbreChambres = "number chambre is required";
+        isValid = false;
+      }
+      if (!formData.nbreSalleBain.trim() === "") {
+        errors.nbreSalleBain = "number salle de bien is required";
+        isValid = false;
+      }
+      if (!formData.nbreEtages.trim() === "") {
+        errors.nbreEtages = "Numero etage salle de bien is required";
+        isValid = false;
+      }
+    }
+
+    setFormErrors(errors);
+    return isValid;
+  };
+
+  return (
+    <section>
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          {currentStep === 1 && (
+            <div>
+              <h2>Step 1: Personal Information</h2>
+              <div className="form-group">
+                <select
+                  name="type"
+                  value={formData.type}
+                  className=""
+                  onChange={handleInputChange}
+                >
+                  <option value="">Select</option>
+                  <option value="l">louer</option>
+                  <option value="V">Vente</option>
+                </select>
+                {formErrors.type && (
+                  <span className="error">{formErrors.type}</span>
+                )}
+              </div>
+              <div className="form-group">
+                {/* <select
+                  name="proprieterType"
+                  value={formData.proprieterType}
+                  className=""
+                  onChange={handleInputChange}
+                >
+                  <option value="">Select</option>
+                  <option value="a">appartement</option>
+                  <option value="m">maison</option>
+                  <option value="v">villa</option>
+                  <option value="r">riad</option>
+                </select> */}
+                {formErrors.proprieterType && (
+                  <span className="error">{formErrors.proprieterType}</span>
+                )}
+              </div>
+            </div>
+          )}
+          {currentStep === 2 && (
+            <div>
+              <h2>Step 2: Details Information</h2>
+              <div className="form-detail">
+                <div className="form-group">
+                  <span>prix</span>
+                  <input
+                    type="number"
+                    name="prix"
+                    placeholder="prix dh"
+                    value={formData.prix}
+                    onChange={handleInputChange}
+                  />
+                  {formErrors.prix && (
+                    <span className="error">{formErrors.prix}</span>
+                  )}
+                </div>
+                <div className="form-group">
+                  <span>surface</span>
+                  <input
+                    type="number"
+                    name="surface"
+                    placeholder="surface m2"
+                    value={formData.surface}
+                    onChange={handleInputChange}
+                  />
+                  {formErrors.surface && (
+                    <span className="error">{formErrors.surface}</span>
+                  )}
+                </div>
+              </div>
+              <div className="form-detail">
+                <div className="form-group">
+                  <span>number chambre</span>
+                  <input
+                    type="number"
+                    name="nbreChambres"
+                    placeholder="number chambre"
+                    value={formData.nbreChambres}
+                    onChange={handleInputChange}
+                  />
+                  {formErrors.nbreChambres && (
+                    <span className="error">{formErrors.nbreChambres}</span>
+                  )}
+                </div>
+                <div className="form-group">
+                  <span>numbere de salle de bien</span>
+                  <input
+                    type="number"
+                    name="nbreSalleBain"
+                    placeholder="numbere de salle de bien"
+                    value={formData.nbreSalleBain}
+                    onChange={handleInputChange}
+                  />
+                  {formErrors.nbreSalleBain && (
+                    <span className="error">{formErrors.nbreSalleBain}</span>
+                  )}
+                </div>
+              </div>
+              <div className="form-detail">
+                <div className="form-group">
+                  <span>Numero etage</span>
+                  <input
+                    type="number"
+                    name="nbreEtages"
+                    placeholder="Numero etage"
+                    value={formData.nbreEtages}
+                    onChange={handleInputChange}
+                  />
+                  {formErrors.nbreEtages && (
+                    <span className="error">{formErrors.nbreEtages}</span>
+                  )}
+                </div>
+                <div className="form-group">
+                  <span>etat proprietaire</span>
+                  <input
+                    type="text"
+                    name="etat"
+                    placeholder="etat proprietaire "
+                    value={formData.etat}
+                    onChange={handleInputChange}
+                  />
+                  {formErrors.etat && (
+                    <span className="error">{formErrors.etat}</span>
+                  )}
+                </div>
+              </div>
+            </div>
+          )}
+          {currentStep === 3 && (
+            <div>
+              <h2>Step 3: maps</h2>
+              <div className="maps">
+                <MapContainer
+                  center={center_position}
+                  zoom={7}
+                  scrollWheelZoom={false}
+                >
+                  <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
+                  <Marker
+                    position={[markerPosition.lat, markerPosition.lng]}
+                    draggable={true}
+                    eventHandlers={{
+                      move: (e) => {
+                        setMarkerPosition(e.target.getLatLng());
+                      },
+                    }}
+                  >
+                    <Popup>
+                      A pretty CSS3 popup. <br /> Easily customizable. Marker
+                      Position:
+                    </Popup>
+                  </Marker>
+                </MapContainer>
+                <div>
+                  <div>
+                    <select id="region" name="region" className="region">
+                      <option value="option1-1">Option 1-1</option>
+                      <option value="option1-2">Option 1-2</option>
+                      <option value="option1-3">Option 1-3</option>
+                    </select>
+                  </div>
+                  <div>
+                    <select id="optvilleion1" name="ville" className="ville">
+                      <option value="option1-1">Option 1-1</option>
+                      <option value="option1-2">Option 1-2</option>
+                      <option value="option1-3">Option 1-3</option>
+                    </select>
+                  </div>
+                  <input
+                    type="text"
+                    placeholder="adresse"
+                    className="adresse"
+                  />
+                </div>
+              </div>
+            </div>
+          )}
+          {currentStep === 4 && (
+            <div>
+              <h2>Step 4: title</h2>
+              <input type="text" placeholder="adresse" className="adresse" />
+              <textarea
+                name="dexcription"
+                id=""
+                placeholder="dexcription"
+                cols="20"
+                rows="6"
+              ></textarea>
+            </div>
+          )}
+          {currentStep === 5 && (
+            <div>
+              <h2>Step 5: images</h2>
+              {currentStep === 5 && (
+                <div>
+                  <div className="">
+                    <label htmlFor="image" className="">
+                      Select an image:
+                    </label>
+                    <input
+                      type="file"
+                      id="image"
+                      name="image"
+                      multiple
+                      accept="image/jpeg,image/png, image/jpg"
+                      className=""
+                      onChange={handleFileChange}
+                    />
+                  </div>
+                </div>
+              )}
+            </div>
+          )}
+          {currentStep === 6 && (
+            <div>
+              <h2>Step 6: Confirmation</h2>
+              <p>AnonceType: {formData.AnonceType}</p>
+              <p>proprieterType: {formData.proprieterType}</p>
+              <p>prix: {formData.prix}</p>
+              <p>surface: {formData.surface}</p>
+              <p>nbreChambres: {formData.nbreChambres}</p>
+              <p>nbreSalleBain: {formData.nbreSalleBain}</p>
+              <p>nbreSalleBain: [markerPosition.lat, markerPosition.lng]</p>
+            </div>
+          )}
+          <div className="button-group">
+            {currentStep > 1 && (
+              <button
+                type="button"
+                className="prev-button"
+                onClick={handlePrevious}
+              >
+                Previous
+              </button>
+            )}
+            {currentStep < 6 ? (
+              <button
+                type="button"
+                className="next-button "
+                onClick={handleNext}
+              >
+                Next
+              </button>
+            ) : (
+              <button type="submit" className="submit-button">
+                Submit
+              </button>
+            )}
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+export default Form;
