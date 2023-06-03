ecb62ea936de40049a95c95c27d66d826e3c59cc
khalidelhamry1 <kindly123klide@gmail.com>
2023-06-03 12:45:46 +0100

    add admin page without styling

---
+++ src/Pages/AdminPage.jsx
@@ -0,0 +1,30 @@
+import Sidebar from "./Sidebar";
+// import Dashboard from "./Dashboard";
+import UserAnonce from "./UserAnonce";
+import { useState } from "react";
+import css from "./AdminPage.module.css";
+import Header from "../components/Header";
+// import Utilisateur from "./Utilisateur";
+
+const AdminPage = () => {
+  const [activeItem, setActiveItem] = useState("dashboard");
+
+  const handleItemClick = (item) => {
+    setActiveItem(item);
+  };
+  return (
+    <>
+      <Header></Header>
+      <div className={css.admin_page}>
+        <Sidebar activeItem={activeItem} onItemClick={handleItemClick} />
+        <div className={css.admin_content}>
+          {/* {activeItem === "dashboard" ? <Dashboard /> : <></>} */}
+          {activeItem === "anonce" ? <UserAnonce /> : <></>}
+          {/* {activeItem === "Utilisateur" ? <Utilisateur /> : <></>} */}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AdminPage;
